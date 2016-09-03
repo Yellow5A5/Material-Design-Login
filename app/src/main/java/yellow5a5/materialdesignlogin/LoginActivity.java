db85383fb0f8b2cbad11a809486e303c48557f6d
Yellow5A5 <scnu.hww@foxmail.com>
2016-09-03 16:52:15 +0800

    Add: add the text to show the process.

--- app/src/main/java/yellow5a5/materialdesignlogin/LoginActivity.java
+++ app/src/main/java/yellow5a5/materialdesignlogin/LoginActivity.java
@@ -2,12 +2,14 @@ package yellow5a5.materialdesignlogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import yellow5a5.materialdesignlogin.View.CatchScrollLayout;
 import yellow5a5.materialdesignlogin.View.SignUpContainer;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView mProcessShowTv;
     private CatchScrollLayout mCatchScrollLayout;
     private SignUpContainer mSignUpContainer;
 
@@ -19,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        mProcessShowTv = (TextView) findViewById(R.id.process_show);
         mCatchScrollLayout = (CatchScrollLayout) findViewById(R.id.catch_sroll_layout);
         mSignUpContainer = (SignUpContainer) findViewById(R.id.sign_up_container);
 
@@ -29,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
                     process = 100 - process;
                 }
                 mSignUpContainer.setAnimProportion(process);
+                mProcessShowTv.setText("Process:" + process + "  isLeft:" + String.valueOf(isLeft));
             }
         });
 
